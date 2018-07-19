6bdbdd690b1c89cf9228bc3029c949d7fb86c1f0
Alex Derr <36800714+AlexDerr@users.noreply.github.com>
2018-07-19 18:07:58 -0500

    Update AListGame.cpp
    Solution to A List game
    
    https://open.kattis.com/problems/listgame

--- Medium/AListGame.cpp
+++ Medium/AListGame.cpp
@@ -20,31 +20,3 @@ int main() {
 	total++;
 	cout << total;
 }
-
-//int main() {
-//	int x, divisor, temp;
-//	int total = 0;
-//
-//	cin >> x;
-//
-//	if (x % 2 == 0) {
-//		while (x != 1) {
-//			x /= 2;
-//			total++;
-//		}
-//		cout << total;
-//	}
-//	else {
-//		while (x != 1) {
-//			divisor = 2;
-//			while (x % divisor != 0 && divisor <= x) {
-//				divisor++;
-//			}
-//			x = x / divisor;
-//			total++;
-//		}
-//		cout << total;
-//	}
-//
-//	return 0;
-//}
