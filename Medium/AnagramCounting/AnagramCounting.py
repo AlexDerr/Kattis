e682fe32d7f9fed5c638da812601d971199794f6
Alex Derr <36800714+AlexDerr@users.noreply.github.com>
2018-07-24 11:00:01 -0500

    Update and rename PythonApplication1.py to AnagramCounting.py

---
+++ Medium/AnagramCounting/AnagramCounting.py
@@ -0,0 +1,27 @@
+import math
+import string
+import sys
+
+occurences = {}
+sentinel = ''
+
+# for line in '\n'.join(iter(input, sentinel)):
+for line in sys.stdin:
+    str = line.splitlines()[0]
+    denominator = 1
+
+    for char in string.ascii_letters:
+        occurences[char] = 0
+
+    for char in str:
+        occurences[char] += 1
+
+    for item in occurences:
+        if occurences[item] != 0:
+            denominator *= math.factorial(occurences[item])
+
+    result = math.factorial(len(str))
+
+    result = result // denominator
+
+    print ( result )
