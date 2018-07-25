3493548dee5a706c1597570d31393814e599885d
Alex Derr <36800714+AlexDerr@users.noreply.github.com>
2018-07-25 16:04:05 -0500

    Create Bijele.cpp

---
+++ Easy/Bijele/Bijele.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int k, q, r, b, kn, p;
+
+	cin >> k >> q >> r >> b >> kn >> p;
+
+	cout << 1 - k << " " << 1 - q << " " << 2 - r << " " << 2 - b << " " << 2 - kn << " " << 8 - p;
+
+	return 0;
+}
