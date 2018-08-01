1f55868dcfdc4fcc407fc2eb862df7c5452fe52f
Alex Derr <36800714+AlexDerr@users.noreply.github.com>
2018-08-01 11:05:55 -0500

    Create SolvingForCarrots.cpp

---
+++ Easy/Solving For Carrots/SolvingForCarrots.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	int x, y;
+	string str;
+
+	cin >> x >> y;
+	for(int i = 0; i < x; i++){
+		cin >> str;
+	}
+
+	cout << y;
+
+	return 0;
+}
