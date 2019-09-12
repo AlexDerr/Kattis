97864bbf2f989a6055c9099535b65b563271633d
Alex Derr <36800714+AlexDerr@users.noreply.github.com>
2019-09-11 22:32:26 -0500

    Create Multi-ClassBinaryNeuralNetworkForHandwrittenDigits.cpp

---
+++ Easy/Multi-Class Binary Neural Network for Handwritten Digits/Multi-ClassBinaryNeuralNetworkForHandwrittenDigits.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    for(int i = 0; i < 150; i++){
+        for(int j = 0; j < 51; j++){
+            cout << -1 << " ";
+        }
+        cout << endl;
+    }
+}
