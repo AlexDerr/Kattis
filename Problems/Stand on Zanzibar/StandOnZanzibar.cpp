7fd2d5354cdd3741ac019eb719ba298fa4522529
Umer Khurshid <umerqurreshi52422@gmail.com>
2022-12-26 22:38:27 +0500

    Update StandOnZanzibar.cpp
    Here is a code i made using functions, i appreciate if you accept it.

--- Problems/Stand on Zanzibar/StandOnZanzibar.cpp
+++ Problems/Stand on Zanzibar/StandOnZanzibar.cpp
@@ -1,27 +1,45 @@
-#include <iostream>
+//https//open.kattis.com/problems/zanzibar
+
+#include<iostream>
+#include<vector>
 using namespace std;
+
+
+
+int StandOnZanzibar(int prev,int temp)
+{
+	int sum = 0;
+	
+		if (temp > (prev * 2))
+			sum = sum + (temp - (prev * 2))	;
+	
+
+	return sum;
+}
 
 int main()
 {
-	int t, temp, prev, total;
-
+	
+	
+	int t, temp,prev;
 	cin >> t;
 
-	for (int i = 0; i < t; i++) {
-		total = 0;
+	for (int i = 0; i < t; i++)
+	{
+		int sum = 0;
 		temp = 1;
-		
-		while (temp != 0) {
+		while (temp != 0)
+		{
 			prev = temp;
 			cin >> temp;
-
-			if (prev * 2 < temp) {
-				total += temp - (prev * 2);
-			}
+			sum = sum + StandOnZanzibar(prev, temp);
 		}
-
-		cout << total << endl;
+		cout << sum << endl;
 	}
+
+	
+
 
 	return 0;
 }
+
